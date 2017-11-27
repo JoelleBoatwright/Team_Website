482ffda3f1c0f4f0b1494c18fe23607d700e15c3
Taelynn A Knutson <tk62539@srcs.loc>
2017-11-27 11:55:02 -0800

    random greetings

---
+++ functions.js
@@ -0,0 +1,13 @@
+//This is where we ill write all of our functions
+
+
+function randomGreetings(){
+  var greetings = ['Hola', 'Здравствуйте', 'Hallo', 'zdravo', 'Bonjour', 'नमस्ते'];
+  var randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+    return randomGreeting;
+}
+
+
+
+
+
